62f7327d651dc51cd9b19e443151a34cc04c1817
Missworld1 <112314838+Missworld1@users.noreply.github.com>
2022-09-27 22:51:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -37,14 +37,14 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("😁 𝗛𝗲𝘆 𝗙𝗿𝗶𝗲𝗻𝗱,𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗬𝗼𝘂𝗿𝘀𝗲𝗹𝗳.", show_alert=True)
+        return await query.answer("😁 𝗜𝗱𝗵𝘂𝗹𝗮𝘆𝗮𝗮𝗰𝗵𝘂𝗺 𝘀𝗼𝗻𝗱𝗵𝗮𝗺𝗮 𝘆𝗼𝘀𝗶𝗻𝗴𝗮 𝗺𝗮𝗮𝗽𝗹𝗮.", show_alert=True)
     try:
         offset = int(offset)
     except:
         offset = 0
     search = BUTTONS.get(key)
     if not search:
-        await query.answer("𝐋𝐢𝐧𝐤 𝐄𝐱𝐩𝐢𝐫𝐞𝐝 𝐊𝐢𝐧𝐝𝐥𝐲 𝐏𝐥𝐞𝐚𝐬𝐞 𝐒𝐞𝐚𝐫𝐜𝐡 𝐀𝐠𝐚𝐢𝐧 🙂.",show_alert=True)
+        await query.answer("𝗜𝗱𝗵𝘂 𝘀𝗮𝗿𝗶𝗽𝗮𝘁𝘁𝘂 𝘃𝗮𝗿𝗮𝗱𝗵𝘂 𝗲𝗹𝗹𝗮 𝗸𝗼𝘁𝘁𝗮𝘆𝘂𝗺 𝗮𝗹𝗶𝗰𝗵𝘂𝘁𝘁𝘂 𝗺𝗼𝗱𝗵𝗮𝗹𝗲𝗿𝘂𝗻𝗱𝗵𝘂 𝘁𝗵𝗲𝗱𝘂 🙂.",show_alert=True)
         return
 
     files, n_offset, total = await get_search_results(search, offset=offset, filter=True)
@@ -110,12 +110,12 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("😁 𝗛𝗲𝘆 𝗙𝗿𝗶𝗲𝗻𝗱,𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗬𝗼𝘂𝗿𝘀𝗲𝗹𝗳.", show_alert=True)
+        return await query.answer("😁 𝗜𝗱𝗵𝘂𝗹𝗮𝘆𝗮𝗮𝗰𝗵𝘂𝗺 𝘀𝗼𝗻𝗱𝗵𝗮𝗺𝗮 𝘆𝗼𝘀𝗶𝗻𝗴𝗮 𝗺𝗮𝗮𝗽𝗹𝗮.", show_alert=True)
     if movie_  == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
     if not movies:
-        return await query.answer("𝐋𝐢𝐧𝐤 𝐄𝐱𝐩𝐢𝐫𝐞𝐝 𝐊𝐢𝐧𝐝𝐥𝐲 𝐏𝐥𝐞𝐚𝐬𝐞 𝐒𝐞𝐚𝐫𝐜𝐡 𝐀𝐠𝐚𝐢𝐧 🙂.", show_alert=True)
+        return await query.answer("𝗜𝗱𝗵𝘂 𝘀𝗮𝗿𝗶𝗽𝗮𝘁𝘁𝘂 𝘃𝗮𝗿𝗮𝗱𝗵𝘂 𝗲𝗹𝗹𝗮 𝗸𝗼𝘁𝘁𝗮𝘆𝘂𝗺 𝗮𝗹𝗶𝗰𝗵𝘂𝘁𝘁𝘂 𝗺𝗼𝗱𝗵𝗮𝗹𝗲𝗿𝘂𝗻𝗱𝗵𝘂 𝘁𝗵𝗲𝗱𝘂 🙂.", show_alert=True)
     movie = movies[(int(movie_))]
     await query.answer('Checking For Movie In Database...')
     k = await manual_filters(bot, query.message, text=movie)
@@ -125,7 +125,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('𝚃𝙷𝙸𝚂 𝙼𝙾𝚅𝙸𝙴 I𝚂 𝙽𝙾𝚃 𝚈𝙴𝚃 𝚁𝙴𝙻𝙴𝙰𝚂𝙴𝙳 𝙾𝚁 𝙰𝙳𝙳𝙴𝙳 𝚃𝙾 𝙳𝙰𝚃𝚂𝙱𝙰𝚂𝙴💌')
+            k = await query.message.edit('𝐊𝐨𝐜𝐡𝐮𝐤𝐚𝐝𝐡𝐚 𝐯𝐫𝐨 𝐚𝐝𝐡𝐮 𝐢𝐧𝐠𝐚 𝐢𝐥𝐥𝐚💌')
             await asyncio.sleep(10)
             await k.delete()
 
@@ -367,7 +367,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("🥺𝗙𝗿𝗶𝗲𝗻𝗱.. 𝗧𝗵𝗮𝘁'𝘀 𝗡𝗼𝘁 𝗙𝗮𝗶𝗿 😓𝗣𝗹𝗲𝗮𝘀𝗲 𝗝𝗼𝗶𝗻 𝗧𝗵𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹..🥺",show_alert=True)
+            await query.answer("🥺𝗠𝗮𝗮𝗽𝗹𝗮.. 𝗜𝗱𝗵𝗲𝗹𝗹𝗮𝗺 𝗻𝗶𝘆𝗮𝗮𝘆𝗮𝗺𝗮 😓𝗢𝗹𝘂𝗻𝗴𝗮 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 𝗹𝗮 𝗷𝗼𝗶𝗻 𝗽𝗮𝗻𝗻𝘂..🥺",show_alert=True)
             return
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
